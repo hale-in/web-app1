721f9661f9ea29bec92dbaba6be220d9553f4edb
hale-in <78592027+hale-in@users.noreply.github.com>
2021-07-14 14:59:11 +0900

    7/14 3ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,9 +2,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, request, HttpResponseRedirect
 from django.shortcuts import render
-
-
-# Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
 
