1428e2ca7b6b57cf378701f68cb70e0734cabdd6
hale-in <78592027+hale-in@users.noreply.github.com>
2021-07-21 10:36:32 +0900

    7/20 3ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, request, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import NewModel
 
@@ -37,6 +37,12 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = UserCreationForm
+    context_object_name = "target_user"
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = 'accountapp/update.html'
 
 
 
