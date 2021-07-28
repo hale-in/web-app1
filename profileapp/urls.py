6383b84573f5723c6f999ff95f6bf7b5f350264d
hale-in <78592027+hale-in@users.noreply.github.com>
2021-07-28 15:22:38 +0900

    7/28 3ban class commit 2

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateview
+from profileapp.views import ProfileCreateview, ProfileUpdateView
+
 app_name = 'profileapp'
 urlpatterns = [
     path('create/', ProfileCreateview.as_view(), name='create'),
-
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]
 
