ac19b7dafcf2cbc1cb175e5ce223bc27d15e58fd
hale-in <78592027+hale-in@users.noreply.github.com>
2021-09-28 10:43:03 +0900

    9/28 3ban class commit 1

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,20 @@ FROM python:3.9.0
 
 WORKDIR /home/
 
+RUN echo 'chunsoccute'
+
 RUN git clone https://github.com/hale-in/web-app1.git
 
 WORKDIR /home/web-app1/
 
-RUN echo "SECRET_KEY=django-insecure-+ba@zl&_h6fy#4z352f83ud53gmkkp*l&be)u+0umpymegtxm+'" > .env
+#RUN echo "SECRET_KEY=django-insecure-+ba@zl&_h6fy#4z352f83ud53gmkkp*l&be)u+0umpymegtxm+'" > .env
 
 RUN pip install -r requirements.txt
 
-RUN python manage.py migrate
+RUN pip install gunicorn
+
+RUN pip install mysqlclient
 
 EXPOSE 8000
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["bash", "-c", "python manage.py collectstatic --noinput --settings=gwangya1.settings.deploy && python manage.py migrate --settings=gwangya1.settings.deploy && gunicorn --env DJANGO_SETTINGS_MODULE=gwangya1.settings.deploy gwangya1.wsgi --bind 0.0.0.0:8000"]
--- gwangya1/settings/deploy.py
+++ gwangya1/settings/deploy.py
@@ -1,20 +1,14 @@
 from .base import *
 
 
-env_list = dict()
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.lstrip().rstrip()
+    file.close()
+    return secret
 
-local_env = open(os.path.join(BASE_DIR, '.env'))
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
-
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secret('django_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -32,8 +26,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'qlalfqjsgh@',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
